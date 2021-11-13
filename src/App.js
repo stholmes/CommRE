fdefc2c7b19436349a39d97fdca76c5945afd2d1
stholmes <stephenshigemi@gmail.com>
2021-11-13 15:45:21 -0800

    Add header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="row">
+          <header>
+            <h1>Data Analysis for CommRE</h1>
+          </header>
+
+        </div>
+
+      </div>
     </div>
   );
 }
