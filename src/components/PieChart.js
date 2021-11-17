bc6670d9a9e31e618bc59d98734d08a48cd64c2a
stholmes <stephenshigemi@gmail.com>
2021-11-17 11:20:00 -0800

    Sort data array for consistent slice coloring

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,12 +1,25 @@
 import { Chart } from "react-google-charts";
 
 const PieChart = props => {
+    // Build data array for chart
     const salesDataKeys = Object.keys(props.salesData);
-    const dataArr = [['Type', 'Sales']];
+    const dataArr = [];
     for(let key of salesDataKeys){
         const typeArr = [key, props.salesData[key]];
         dataArr.push(typeArr);
     }
+    // Sort data array for consistant slice coloring
+    dataArr.sort((a,b)=>{
+        if(a[0] > b[0]){
+            return 1
+        }else if (a[0] < b[0]){
+            return -1
+        }else{
+            return 0
+        } 
+    })
+    dataArr.unshift(['Type', 'Sales'])
+    
     return (
         <div className="">
             <h3 className="d-md-none text-center">Sales by Property Type for {props.name}</h3>
@@ -16,7 +29,7 @@ const PieChart = props => {
                 chartType="PieChart"
                 loader={<div>Loading Chart</div>}
                 data={dataArr}
-                // legend.position = {{position: 'bottom', alignment: 'start'}}
+                
                 options = {{
                     legend: 'bottom',
                     chartArea: {'width': '100%', 'height': '80%'}
