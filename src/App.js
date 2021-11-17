6b256341795e798c473a74c98324b29f2189fd4a
stholmes <stephenshigemi@gmail.com>
2021-11-16 18:20:40 -0800

    Add test data and process for aggregation for analysis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
 import Table from './components/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const dummyData = [
+  {agent: 'steve', propertyType:'Office'},
+  {agent: 'steve', propertyType:'Land'},
+  {agent: 'steve', propertyType:'Residential'},
+  {agent: 'steve', propertyType:'Office'},
+  {agent: 'steve', propertyType:'Office'},
+  {agent: 'steve', propertyType:'Land'},
+  {agent: 'steve', propertyType:'Land'},
+  {agent: 'steve', propertyType:'Residential'},
+  {agent: 'steve', propertyType:'Residential'},
+  {agent: 'steve', propertyType:'Office'},
+  {agent: 'steve', propertyType:'Land'},
+  {agent: 'steve', propertyType:'Residential'},
+  {agent: 'steve', propertyType:'Residential'},
+  {agent: 'steve', propertyType:'Land'},
+  {agent: 'steve', propertyType:'Office'},
+  {agent: 'steve', propertyType:'Residential'},
+  {agent: 'bob', propertyType:'Land'},
+  {agent: 'bob', propertyType:'Land'},
+  {agent: 'bob', propertyType:'Residential'},
+  {agent: 'bob', propertyType:'Office'},
+  {agent: 'jack', propertyType:'Office'},
+  {agent: 'jack', propertyType:'Office'},
+  {agent: 'jack', propertyType:'Land'},
+  {agent: 'jack', propertyType:'Residential'},
+  {agent: 'jack', propertyType:'Office'},
+];
 
 const App = () => {
+  const salesByAgent = [];
+  const salesDataIterable = dummyData.values();
+
+  for ( const sale of salesDataIterable){
+    let agentName = sale.agent;
+    let propertyType = sale.propertyType;
+
+    if (salesByAgent.findIndex(sale => sale.name === agentName) === -1){
+      let agentObj = {};
+      agentObj.name = agentName;
+      agentObj.totalSales = 1;
+      agentObj.sales = {};
+      agentObj.sales[propertyType] = 1;
+      salesByAgent.push(agentObj);
+    }else{
+      let index = salesByAgent.findIndex(agent => agent.name === agentName);
+
+      if (salesByAgent[index].sales[propertyType] === undefined){
+        salesByAgent[index].sales[propertyType] = 1;
+      }else{
+        salesByAgent[index].sales[propertyType] += 1;
+      };
+      salesByAgent[index].totalSales += 1;
+    };
+  };
+
+  console.log(salesByAgent)
+
+
   return (
     <div className="App">
       <div className="container">
